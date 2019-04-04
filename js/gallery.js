199a974ff0809f380b8bc7876d387d03c02df460
Code Monkey <code.monkey@codemonkey.com>
2019-04-04 14:52:01 -0400

    pick up at 11.4.2 Exercises

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,25 @@
 function activateGallery() {
-  let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
+  let currentClass = "current";
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click",function() {
+      //set the image in the middle pane to the clicked image in the left pane.
       mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      //set the clicked image in the left pane to have the highlighting around it signifying that it is the current image.
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      //set the title and description in the far right pane to match the clicked image.
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
